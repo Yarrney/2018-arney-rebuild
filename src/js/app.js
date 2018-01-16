efb46faccb352df2c7bc451a3fbb9174fbe2cec1
Yarrney <alex.arney@gmail.com>
2018-01-16 20:59:51 +1100

    still setting up webpack

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,8 @@
 // CSS
 require('../css/main.css');
 
+// SASS
+require('../sass/styles.scss');
+
 // JavaScript
 require('./main.js');
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,61 @@
 module.exports = {
-  entry: './src/js/app.js',
-  output: {
-    path: __dirname + '/dist',
-    filename: 'bundle.js'
+	entry: './src/js/app.js',
+	output: {
+		path: __dirname + '/dist',
+		filename: 'bundle.js'
+	},
+
+  	module: {
+    	rules: [
+
+			{ 	// Install CSS Loaders
+				test: /\.(css|scss)$/,
+				exclude: /node_modules/,
+				loaders: [
+				  'style-loader',
+				  'css-loader',
+				  'sass-loader'
+				]
+			},
+
+			{ 	// Install URL Loader
+				test: /\.(svg|gif|png|eot|woff|ttf)$/,
+				loaders: [
+					'url-loader'
+				]
+			},
+
+			{ 	// Babel es2015 
+				test: /\.js$/,
+				loader: 'babel-loader?presets[]=es2015'
+			}
+
+    ]
   }
+
+};
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1,
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader'
+                    }
+                ]
+            }
+        ]
+    }
 }
